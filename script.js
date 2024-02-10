b43b19472624aff498b8f90075dd6798a2ac53c6
Ahmed <120523951+EruditeDeveloper@users.noreply.github.com>
2024-02-10 15:46:00 +0000

    project complete

--- script.js
+++ script.js
@@ -1,60 +1,75 @@
 function getRandomInt() {
-    return Math.floor(Math.random() * 3);
+	return Math.floor(Math.random() * 3);
 }
 
 function getComputerChoice() {
-    // will randomly return rock paper or scissors
-    const compChoiceList = ["rock", "paper", "scissors"];
+	// will randomly return rock paper or scissors
+	const compChoiceList = ["rock", "paper", "scissors"];
 
-    let compChoice = compChoiceList[getRandomInt()];
-    return compChoice;
+	let compChoice = compChoiceList[getRandomInt()];
+	return compChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    // play 1 round
-    if (
-        playerSelection !== "rock" &&
-        playerSelection !== "paper" &&
-        playerSelection !== "scissors"
-    ) {
-        return [null, "Invalid choice. Please select either rock, paper, or scissors."];
-    }
+	// play 1 round
+	if (
+		playerSelection !== "rock" &&
+		playerSelection !== "paper" &&
+		playerSelection !== "scissors"
+	) {
+		return [
+			null,
+			"Invalid choice. Please select either rock, paper, or scissors.",
+		];
+	}
 
-    if (playerSelection === computerSelection) {
-        return [null, `Draw! Player chose: ${playerSelection} Computer chose: ${computerSelection}`];
-    }
+	if (playerSelection === computerSelection) {
+		return [
+			null,
+			`Draw! Player chose: ${playerSelection} Computer chose: ${computerSelection}`,
+		];
+	}
 
-    if (
-        (playerSelection === "rock" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "rock")
-    ) {
-        return [false, `Computer wins! Player chose: ${playerSelection} Computer chose: ${computerSelection}`];
-    } else {
-        return [true, `Player wins! Player chose: ${playerSelection} Computer chose: ${computerSelection}`];
-    }
+	if (
+		(playerSelection === "rock" && computerSelection === "paper") ||
+		(playerSelection === "paper" && computerSelection === "scissors") ||
+		(playerSelection === "scissors" && computerSelection === "rock")
+	) {
+		return [
+			false,
+			`Computer wins! Player chose: ${playerSelection} Computer chose: ${computerSelection}`,
+		];
+	} else {
+		return [
+			true,
+			`Player wins! Player chose: ${playerSelection} Computer chose: ${computerSelection}`,
+		];
+	}
 }
-
-let playerScore = 0;
-let computerScore = 0;
 
 function playGame(playerChoice, computerChoice) {
+	let playerScore = 0;
+	let computerScore = 0;
 
-    let roundResults = playRound(playerChoice, computerChoice);
+	for (let i = 0; i < 5; i++) {
+		playerChoice = prompt("enter either rock paper or scissors");
+		computerChoice = getComputerChoice();
+		
+		let roundResults = playRound(playerChoice, computerChoice);
+		console.log(roundResults);
+		if (roundResults[0] === true) {
+			playerScore++;
+		} else {
+			computerScore++;
+		}
 
-    if (roundResults[0] === true) {
-        console.log(roundResults);
-        playerScore ++;
-    } else {
-        console.log(roundResults);
-        computerScore ++;
-    }
+		
+	}
 
-    return (`Player score: ${playerScore}, Computer score: ${computerScore}`)
+	return `Player score: ${playerScore}, Computer score: ${computerScore}`;
 }
 
+let playerSelection = "";
+let computerSelection = getComputerChoice();
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice()
-
-console.log(playGame(playerSelection, computerSelection));
+console.log(playGame());
